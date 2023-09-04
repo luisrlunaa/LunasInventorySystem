eea35291efe151d0d26c7cd2de56aaa8c339fc0b
Jordhano <Jordhanomx@gmail.com>
2023-09-03 22:33:33 -0400

    Add Location

---
+++ src/Infrastructure/Localization/LocalizationSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Luna.Infrastructure.Localization
+{
+    public class LocalizationSettings
+    {
+
+        public bool? EnableLocalization { get; set; }
+        public string? ResourcesPath { get; set; }
+        public ReadOnlyCollection<string>? SupportedCultures { get; set; }
+        public string? DefaultRequestCulture { get; set; }
+        public bool? FallBackToParent { get; set; }
+    }
+}
---
+++ src/Infrastructure/Localization/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+
+namespace Luna.Infrastructure.Localization;
+
+internal static class Startup
+{
+    internal static IServiceCollection AddLocalization(this IServiceCollection services, IConfiguration config)
+    {
+        var localizationSettings = config.GetSection(nameof(LocalizationSettings)).Get<LocalizationSettings>();
+
+        if (localizationSettings?.EnableLocalization is true
+            && localizationSettings.ResourcesPath is not null)
+        {
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                if (localizationSettings.SupportedCultures != null)
+                {
+                    var supportedCultures = localizationSettings.SupportedCultures.Select(x => new CultureInfo(x)).ToList();
+
+                    options.SupportedCultures = supportedCultures;
+                    options.SupportedUICultures = supportedCultures;
+                }
+
+                options.DefaultRequestCulture = new RequestCulture(localizationSettings.DefaultRequestCulture ?? "en-US");
+                options.FallBackToParentCultures = localizationSettings.FallBackToParent ?? true;
+                options.FallBackToParentUICultures = localizationSettings.FallBackToParent ?? true;
+            });
+
+        }
+
+        return services;
+    }
+}
